8e76654f4b68aaddf69e6094e79c84c75b27f6e4
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-08-02 17:05:51 +0200

    updated error messages and removed the non-secure rng function

--- insiders/src/index.ts
+++ insiders/src/index.ts
@@ -35,19 +35,13 @@ function smartID(use_case: "user_id" | "device_id" | "product_id" | "session_tok
     }
 }
 
-
-function NON_SECURE_RNG(): number  {
-    console.warn("WARNING: Using non-secure random number generator. This random number generator is NOT cryptographically secure and is not recommended for production use.");
-    return Number(Math.random().toString().replace("0.", ""));
-}
-
 function SECURE_RNG(): number | undefined{
     if (typeof crypto !== "undefined" && crypto.getRandomValues) {
         const array = new Uint32Array(1);
         crypto.getRandomValues(array);
         return array[0];
     } else {
-        throw new Error("Secure random number generator is not available in this environment.");
+        throw new Error("The crypto library, powering the secure random number generator, is not available in this environment. We urge you to fix this issue, as the absence of the crypto library can often indicate that the environment is not secure.");
     }
 }
 
@@ -80,10 +74,7 @@ const idtools = {
         "v5": v5,
         "NIL": NIL
     },
-    "randomNumber": {
-        "default": NON_SECURE_RNG,
-        "secure": SECURE_RNG
-    },
+    "randomNumber": SECURE_RNG,
     "smart": smartID,
     "insiderFeatures": {
         "hex128": Hex128Bit,
