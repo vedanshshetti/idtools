ee20304ec86ff8a2a0b5bc709f7e7f1bd33a649a
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-08-02 17:11:36 +0200

    removed non-secure rng

--- prod/src/index.ts
+++ prod/src/index.ts
@@ -36,11 +36,6 @@ function smartID(use_case: "user_id" | "device_id" | "product_id" | "session_tok
 }
 
 
-function NON_SECURE_RNG(): number  {
-    console.warn("WARNING: Using non-secure random number generator. This random number generator is NOT cryptographically secure and is not recommended for production use.");
-    return Number(Math.random().toString().replace("0.", ""));
-}
-
 function SECURE_RNG(): number | undefined{
     if (typeof crypto !== "undefined" && crypto.getRandomValues) {
         const array = new Uint32Array(1);
@@ -63,10 +58,7 @@ const idtools = {
         "v5": v5,
         "NIL": NIL
     },
-    "randomNumber": {
-        "default": NON_SECURE_RNG,
-        "secure": SECURE_RNG
-    },
+    "randomNumber": SECURE_RNG,
     "smart": smartID
 }
 
